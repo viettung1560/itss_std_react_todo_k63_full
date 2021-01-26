ece9494fa81bb6206e7d13fb7a1e07f398b56ae5
EC2 Default User <ec2-user@ip-172-31-45-184.ap-southeast-1.compute.internal>
2021-01-26 07:57:14 +0000

    Modify TodoItem component

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -26,10 +26,10 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       {items.map(item => (
-        <label className="panel-block">
-            <input type="checkbox" />
-            {item.text}
-        </label>
+        <TodoItem 
+          key={item.key}
+          item={item} 
+        />
       ))}
       <div className="panel-block">
         {items.length} items
