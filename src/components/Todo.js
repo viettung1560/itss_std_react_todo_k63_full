63d0b5e7dee40c7a00dd97f2d876becc11ae1450
EC2 Default User <ec2-user@ip-172-31-45-184.ap-southeast-1.compute.internal>
2021-01-26 10:10:40 +0000

    modify input field

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -36,12 +36,17 @@ function Todo() {
     });
     putItems(newItems);
   };
+  
+  const handleAdd = text => {
+    putItems([...items, { key: getKey(), text, done: false }]);
+  };
 
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
+      <Input onAdd={handleAdd} />
       {items.map(item => (
         <TodoItem 
           key={item.key}
