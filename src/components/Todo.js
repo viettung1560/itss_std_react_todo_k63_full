1892c10ee29c53ef6333a5d908290be1b1bab2e4
EC2 Default User <ec2-user@ip-172-31-45-184.ap-southeast-1.compute.internal>
2021-01-27 03:46:39 +0000

    storage todo to localstorage

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -19,13 +19,14 @@ import useStorage from '../hooks/storage';
 import {getKey} from "../lib/util";
 
 function Todo() {
-  const [items, putItems] = React.useState([
-      /* テストコード 開始 */
-    { key: getKey(), text: '日本語の宿題', done: false },
-    { key: getKey(), text: 'reactを勉強する', done: false },
-    { key: getKey(), text: '明日の準備をする', done: false },
-    /* テストコード 終了 */
-  ]);
+  const [items, putItems, clearItems] = useStorage();
+  // const [items, putItems] = React.useState([
+  //     /* テストコード 開始 */
+  //   { key: getKey(), text: '日本語の宿題', done: false },
+  //   { key: getKey(), text: 'reactを勉強する', done: false },
+  //   { key: getKey(), text: '明日の準備をする', done: false },
+  //   /* テストコード 終了 */
+  // ]);
   
   const [filter, setFilter] = React.useState('ALL');
 
@@ -34,7 +35,6 @@ function Todo() {
     if (filter === 'TODO') return !item.done;
     if (filter === 'DONE') return item.done;
   });
-
   
   const handleCheck = checked => {
     const newItems = items.map(item => {
@@ -71,6 +71,11 @@ function Todo() {
       ))}
       <div className="panel-block">
         {displayItems.length} items
+      </div>
+      <div className="panel-block">
+        <button className="button is-light is-fullwidth" onClick={clearItems}>
+          全てのToDoを削除
+        </button>
       </div>
     </div>
   );
