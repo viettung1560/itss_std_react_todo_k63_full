fb26be90d6ee1b616ae7faec59b64fa05fbf75bc
EC2 Default User <ec2-user@ip-172-31-45-184.ap-southeast-1.compute.internal>
2021-01-27 03:31:40 +0000

    mofidy filter as Tab

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -27,6 +27,15 @@ function Todo() {
     /* テストコード 終了 */
   ]);
   
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  
   const handleCheck = checked => {
     const newItems = items.map(item => {
       if (item.key === checked.key) {
@@ -40,6 +49,8 @@ function Todo() {
   const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
+  
+  const handleFilterChange = value => setFilter(value);
 
   return (
     <div className="panel">
@@ -47,7 +58,11 @@ function Todo() {
         ITSS ToDoアプリ
       </div>
       <Input onAdd={handleAdd} />
-      {items.map(item => (
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
         <TodoItem 
           key={item.key}
           item={item}
@@ -55,7 +70,7 @@ function Todo() {
         />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
